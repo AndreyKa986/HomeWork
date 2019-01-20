953b7cb695b9aa5a34357e009c40605d6be7c620
AndreyKa986 <letum8658@tut.by>
2019-01-20 14:52:46 +0300

    changeTask2

--- HW/src/lesson1/Task2.java
+++ HW/src/lesson1/Task2.java
@@ -2,8 +2,8 @@ package lesson1;
 
 public class Task2 {
     public static void main(String[] args) {
-        int number = (int) (Math.random() * 10000 + 1);
+        int number = (int) (Math.random() * 20000 + 1) - 10000;
         int i = number % 10;
-        System.out.println(number + "\n" + (i == 7 ? "последнее число 7" : "последнее число не 7"));
+        System.out.println(number + "\n" + (i == 7 || i == -7 ? "последнее число 7" : "последнее число не 7"));
     }
 }
