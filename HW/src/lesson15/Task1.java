8b7dad1249f840a69bef0b402f9cacde39e7e217
AndreyKa986 <letum8658@tut.by>
2019-03-15 18:18:12 +0300

    lesson 15 and final version of 14.2

---
+++ HW/src/lesson15/Task1.java
@@ -0,0 +1,49 @@
+package lesson15;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        StringMaker thread1 = new StringMaker();
+        StringMaker thread2 = new StringMaker();
+        thread1.setName("Thread-1");
+        thread2.setName("Thread-2");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Для запуска приложения введите - start");
+        String string = scanner.nextLine();
+        while (true) {
+            if (!string.equalsIgnoreCase("start")) {
+                System.out.println("Вы ввели - " + string);
+                System.out.println("А надо - start. Введите заново:");
+                string = scanner.nextLine();
+            } else {
+                thread1.start();
+                thread2.start();
+                break;
+            }
+        }
+    }
+
+    private static void print10(int[] array) {
+        System.out.print(Thread.currentThread().getName());
+        for (int i : array) {
+            System.out.printf("%4d", i);
+        }
+        System.out.println();
+    }
+
+    public static class StringMaker extends Thread {
+        @Override
+        public void run() {
+            int number = 0;
+            for (int i = 0; i < 10; i++) {
+                int[] array = new int[10];
+                for (int j = 0; j < 10; j++) {
+                    array[j] = ++number;
+                }
+                print10(array);
+            }
+        }
+    }
+}
